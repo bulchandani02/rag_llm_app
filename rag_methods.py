8b00a18237c58c44d0119eb7cc0b459c762f38fa
bulchandani02 <Jyotsna.bulchandani02@gmail.com>
2024-10-14 19:45:33 +0000

    Change03

--- rag_methods.py
+++ rag_methods.py
@@ -160,10 +160,10 @@ def get_conversational_rag_chain(llm):
     prompt = ChatPromptTemplate.from_messages([
         ("system",
         """You are a coding and analysis assistant. You will have to answer to user's queries.
-        If user asks to share 'BRE Document' then provide analysis from the context in given format. 
-        1. Overview (Files = file name, Lines - number of lines of code from context file, Business Rules - How many business rules that context has, Test cases - analyse the unit tests from the context and provide number of unit test cases)
-        2. Business Rules (extract business rules from context and explain in bullet points)
-        3. Technical Details (Input Parameters = provide input parameters from conect, Business Logic = provide business logic summary here, Output = provide expected output with examples)
+        If user asks to share 'BRE Document' then provide analysis from the context in given format or sequesnce with exact headlines- 
+        1. "Overview" (Files = file name, Lines - number of lines of code from context file, Business Rules - How many business rules that context has, Test cases - analyse the unit tests from the context and provide number of unit test cases)
+        2. "Business Rules" (extract business rules from context and explain in bullet points)
+        3. "Technical Details" (Input Parameters = provide input parameters from conect, Business Logic = provide business logic summary here, Output = provide expected output with examples)
         You can also use your knowledge to assist answering the user's queries.\n
         {context}"""),
         MessagesPlaceholder(variable_name="messages"),
