cab9266fedc74fa7cdd4510effca7cad19c9eb9b
bulchandani02 <Jyotsna.bulchandani02@gmail.com>
2024-11-07 12:58:10 +0000

    loginapp change 02

--- login-app.py
+++ login-app.py
@@ -3,26 +3,8 @@ from pathlib import Path
 
 import streamlit as st
 import streamlit_authenticator as stauth
-import os
-import dotenv
-import uuid
 
-# check if it's linux so it works on Streamlit Cloud
-if os.name == 'posix':
-    __import__('pysqlite3')
-    import sys
-    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
-from langchain_openai import ChatOpenAI, AzureChatOpenAI
-from langchain_anthropic import ChatAnthropic
-from langchain.schema import HumanMessage, AIMessage
-
-from rag_methods import (
-    load_doc_to_db, 
-    load_url_to_db,
-    stream_llm_response,
-    stream_llm_rag_response,
-)
 
 dotenv.load_dotenv()
 
