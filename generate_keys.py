f33103c092cebc22f0a55b31ce85731b0b145a9a
bulchandani02 <Jyotsna.bulchandani02@gmail.com>
2024-11-07 12:43:55 +0000

    pickle-file

--- generate_keys.py
+++ generate_keys.py
@@ -7,7 +7,7 @@ names = ["Jyotsna Bulchandani", "Shivani Sohaney", "Sreejith Sukumara Menon", "J
 usernames = ["Jyotsna", "Shivani", "Sreejith", "Jaffer"]
 passwords = ["Welcome@123", "Welcome@123", "Welcome@123", "Welcome@123"]
 
-hashed_passwords = stauth.Hasher(passwords).generate()
+hashed_passwords = stauth.Hasher(passwords).hash
 
 file_path = Path(__file__).parent / "hashed_pw.pk1"
 with file_path.open("wb") as file:
